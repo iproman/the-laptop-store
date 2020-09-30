0b6eaece3ec2d48c9a45f53dd8500f43aca7339c
iproman <itechman@yandex.ru>
2020-09-30 12:48:06 +0500

    Listen server.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
+const http = require('http');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'UTF-8');
 const laptopData = JSON.parse(json);
 
+const server = http.createServer((request, res) => {
+})
+
+server.listen(8080, '127.0.0.1', () => {
+    console.log('Started listening for requests now');
+});
