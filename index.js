68a593e74af3eb0d65d1abda5d22c8adb2ce9c6e
iproman <itechman@yandex.ru>
2020-09-30 12:49:47 +0500

    Dynamic replace strings to laptop data.

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ const server = http.createServer((request, res) => {
 server.listen(8080, '127.0.0.1', () => {
     console.log('Started listening for requests now');
 });
+
+function replaceTemplate(originalHtml, laptop) {
+    let output = originalHtml.replace(/{%PRODUCT_NAME%}/g, laptop.productName);
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+
+    return output;
+}
